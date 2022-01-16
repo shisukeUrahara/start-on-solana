c48c997cdd6238c883fa822abd4ad349b933de84
ShisukeUrahara <abhisheksagu@gmail.com>
2022-01-16 19:07:59 +0530

    fixed issues in airdrop project

--- airdrop-project/index.js
+++ airdrop-project/index.js
@@ -1,90 +1,149 @@
-import {
+const  {
   Connection,
-  PublicKey,
   clusterApiUrl,
+  PublicKey,
   Keypair,
   LAMPORTS_PER_SOL,
   Transaction,
-  Account,
-} from "@solana/web3.js";
+  Account
+} = require( "@solana/web3.js");
 
-// creating a new key pair
-const newPair = new Keypair();
-// console.log("**@ newPair is , ", newPair);
+// creating a new user wallet
+const keyPair = new Keypair();
+// console.log("**@ new keypair 0 is , ",keyPair);
 
-// getting the public key from the wallet instance
-const publicKey = new PublicKey(newPair.publicKey).toString();
-// console.log("**@ publicKey is , ", publicKey);
+// printing out public key 
+const publicKey = new PublicKey(keyPair._keypair.publicKey).toString();
+// console.log("**@ public key 0 is , ",publicKey);
 
-// getting the secret key from the wallet instance
-const secretKey = newPair.secretKey;
-// console.log("**@ secretKey is , ", secretKey.toString());
+// printing out private key 
+const privateKey = keyPair._keypair.secretKey;
+// console.log("**@ private key 0 is , ",privateKey);
 
-// a function to get wallet balance
-const getWalletBalance = async () => {
-  try {
-    // connecting to the network
-    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-    // console.log("**@ connection   is , ", connection);
+console.log("*********************************************************************");
+console.log("*********************************************************************");
+console.log("*********************************************************************");
+console.log("*********************************************************************");
 
-    // creating a new wallet
-    const myWallet = await Keypair.fromSecretKey(secretKey);
-    // console.log("**@ myWallet   is , ", myWallet);
 
-    // getting the wallet balance
-    const balance = await connection.getBalance(
-      new PublicKey(myWallet.publicKey)
-    );
-    console.log(
-      "**@ wallet balance  is , ",
-      parseInt(balance) / LAMPORTS_PER_SOL,
-      "SOL"
-    );
+// a function to get wallet balance using private key
+const getWalletBalanceFromPrivateKey = async (privateKey)=>{
+  try{
+    // connecting to solana network 
+    const connection = new Connection(clusterApiUrl("devnet"),"confirmed");
+    // console.log("**@ connection is , ",connection);
 
-    return balance;
-  } catch (err) {
-    console.log("**@ getWalletBalance failed , error is , ", err);
+    // creating a wallet object from the secret key 
+    const userWallet = await Keypair.fromSecretKey(privateKey);
+    // console.log("**@ user wallet 1  is , ",userWallet);
+
+    // getting public key from user wallet 
+    const publicKey = new PublicKey(userWallet.publicKey);
+    // console.log("**@ publicKey 1  is , ",publicKey);
+
+    // getting wallet balance 
+    const walletBalance = await connection.getBalance(publicKey);
+    console.log("**@ wallet balance 1 is , ",walletBalance);
+
+    console.log(`   Wallet balance 1: ${parseInt(walletBalance)/LAMPORTS_PER_SOL}SOL`);
+
+
+
+
+
   }
-};
+  catch(err){
+    console.log("**@ error while getting wallet balance from private key , error is , ",err);
+  }
+}
 
-const airDropSol = async () => {
+
+// a function to get wallet balance using public key 
+const getWalletBalanceFromPublicKey = async (publicKey)=>{
+  try{
+    // connecting to solana network 
+    const connection = new Connection(clusterApiUrl("devnet"),"confirmed");
+    // console.log("**@ connection is , ",connection);
+
+    // console.log("**@ public key 2 is , ",publicKey);
+
+    const walletBalance = await connection.getBalance(publicKey);
+    console.log("**@ walletBalance 2 is , ",walletBalance);
+    console.log(`   Wallet balance 2: ${parseInt(walletBalance)/LAMPORTS_PER_SOL}SOL`);
+
+
+
+  }
+  catch(err){
+    console.log("**@ error while getting wallet balance from public key , error is , ",err);
+  }
+} 
+
+// a function to airdrop sol using private key 
+const airdropSolUsingPrivateKey= async (privateKey)=>{
   try {
-    // connecting to network
-    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-    const walletKeyPair = await Keypair.fromSecretKey(secretKey);
+    // connecting to the solana network 
+    const connection = new Connection(clusterApiUrl("devnet"),"confirmed");
+    
 
-    console.log("**@ AIRDROPPING SOLS ");
+    // generating user wallet from private key 
+    const userWallet = await Keypair.fromSecretKey(privateKey);
 
-    // creating an airdrop signature
-    const fromAirdropSignature = await connection.requestAirdrop(
-      new PublicKey(walletKeyPair.publicKey),
-      5 * LAMPORTS_PER_SOL
+    // generating and signing the tx 
+    const airdropSignature = await connection.requestAirdrop(
+      new PublicKey(userWallet.publicKey),
+      2*LAMPORTS_PER_SOL
     );
 
-    // waiting for confirming the transaction
+    // sending and confirming the tx 
+    await connection.confirmTransaction(airdropSignature)
 
-    // console.log("**@ WAITING FOR CONFIRMATION  ");
-
-    await connection
-      .confirmTransaction(fromAirdropSignature)
-      .then((result) => {
-        console.log("**@ airdrop completed");
-      })
-      .catch((err) => {
-        console.log("**@ confirmTransaction failed , error is , ", err);
-      });
-
-    // console.log("**@ airdrop completed");
-  } catch (err) {
-    console.log("**@ airDropSol failed , error is , ", err);
   }
-};
+  catch(err){
+    console.log("**@ something went wrong while airdropping sol using private key , error is , ",err);
+  }
+}
 
-// making a driver function to call the airdrop method
-const driverFunction = async () => {
-  await getWalletBalance();
-  await airDropSol();
-  await getWalletBalance();
-};
+// a function to airdrop sol using public key 
+const airdropSolUsingPublicKey= async (publicKey)=>{
+  try {
+    // connecting to the solana network 
+    const connection = new Connection(clusterApiUrl("devnet"),"confirmed");
+    
 
-driverFunction();
+
+
+    // generating and signing the tx 
+    const airdropSignature = await connection.requestAirdrop(
+      new PublicKey(publicKey),
+      2*LAMPORTS_PER_SOL
+    );
+
+    // sending and confirming the tx 
+    await connection.confirmTransaction(airdropSignature)
+
+  }
+  catch(err){
+    console.log("**@ something went wrong while airdropping sol using private key , error is , ",err);
+  }
+}
+
+
+// airdrop using public keys 
+const driveFunctionPublic = async ()=>{
+ await  getWalletBalanceFromPublicKey(keyPair.publicKey);
+ await  airdropSolUsingPublicKey(keyPair.publicKey)
+ await  getWalletBalanceFromPublicKey(keyPair.publicKey);
+
+}
+
+
+// airdrop using private keys 
+const driveFunctionPrivate = async ()=>{
+ await getWalletBalanceFromPrivateKey(privateKey);
+ await  airdropSolUsingPrivateKey(privateKey);
+ await  getWalletBalanceFromPrivateKey(privateKey);
+}
+
+// driveFunctionPrivate();
+driveFunctionPublic();
